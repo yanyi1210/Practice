388fc70c264f8ac2201a55fe659d9fd40bdeb4aa
yanyi1210 <yanyi.yvonne@gmail.com>
2015-03-05 23:09:07 -0500

    No.1

--- LeetCode/src/main/java/yy/practice/leetcode/TwoSum.java
+++ LeetCode/src/main/java/yy/practice/leetcode/TwoSum.java
@@ -1,36 +1,49 @@
 package yy.practice.leetcode;
 
+import java.util.HashMap;
+
 import org.junit.Test;
 
 public class TwoSum {
-	
+
 	@Test
-	public void test(){
-		int[] numbers = {-3,4,3,90};
-	    int target=0;
-	    twoSum(numbers,target);
+	public void test() {
+		int[] numbers = { -3, 4, 3, 90 };
+		int target = 0;
+		twoSum(numbers, target);
 	}
-	
-	
-    public int[] twoSum(int[] numbers, int target) {
-        
-        int[] index = null;
-        for(int i=0;i<numbers.length-1;i++){
-            System.out.println("i="+i);
-           
-            for(int j=i+1;j<numbers.length;j++){
-            	System.out.println("j="+j);
-                System.out.print("numbers["+i+"]="+numbers[i]+", numbers["+j+"]="+numbers[j]);
-               
-                if(numbers[i]+numbers[j]==target){
-                	index = new int[2];
-                    index[0] = i;
-                    index[1] = j;
-                    return index;
-                }
-            }
-        }
-        return index;
-    }
+
+	public int[] twoSum(int[] numbers, int target) {
+
+		HashMap<Integer, Integer> hashmap = new HashMap<Integer, Integer>();
+		for (int i = 0; i < numbers.length; i++) {
+			Integer diff = (Integer) (target - numbers[i]);
+			if (hashmap.containsKey(diff)) {
+				int toReturn[] = { hashmap.get(diff) + 1, i + 1 };
+				return toReturn;
+			}
+			hashmap.put(numbers[i], i);
+		}
+		return null;
+	}
+
+	public int[] twoSum2(int[] numbers, int target) {
+
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+		int[] result = new int[2];
+
+		for (int i = 0; i < numbers.length; i++) {
+			if (map.containsKey(numbers[i])) {
+				int index = map.get(numbers[i]);
+				result[0] = index + 1;
+				result[1] = i + 1;
+				break;
+			} else {
+				map.put(target - numbers[i], i);
+			}
+		}
+
+		return result;
+	}
 
 }
